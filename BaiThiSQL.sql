89fdb1b5501e3b0ff8ef70d1410204eeb3045c42
Hongson <hongson31202@gmail.com>
2023-10-19 19:57:46 +0700

    baithi

---
+++ BaiThiSQL.sql
@@ -0,0 +1,34 @@
+﻿CREATE DATABASE BaiThi
+GO
+USE BaiThi
+GO
+
+CREATE TABLE EmployeeTable (
+	EmployeeCode nchar(3) PRIMARY KEY,
+	EmployeeName nvarchar(30),
+	Numberwork int,
+	NumberoffWorkpay int,
+	NumberoffWithoutpay int,
+	Basicsalary int ,
+	Growsalary int,
+	Netsalary int
+)
+CREATE TABLE DepartmentTable (
+	Departmentcode nchar(5)
+)
+CREATE TABLE DepartmentEmployee (
+	IDDepEmp int IDENTITY PRIMARY KEY,
+	EmployeeCode nchar(3),
+	Departmentcode nchar(5)
+)
+
+
+-- insert
+INSERT INTO EmployeeTable (EmployeeCode,EmployeeName,Numberwork,NumberoffWorkpay,NumberoffWithoutpay,Basicsalary,Growsalary,Netsalary)
+VALUES	('A1','Nguyễn Văn A',22,1,0,1000,22000,20000)
+
+INSERT INTO DepartmentTable (Departmentcode)
+VALUES	('IT')
+
+INSERT INTO  DepartmentEmployee
+VALUES ('A1','IT')
